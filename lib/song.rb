9bb82ad732349a6324486d5e846034a4196a0513
Joseph Kao <josephtkao@gmail.com>
2020-04-16 03:24:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,10 @@ class Song
   end
 
   def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
+    this_artist = file.split(" - ")[0]
     name = file.split(" - ")[1]
     song = Song.new(name)
-    song.artist = artist_name
+    song.artist = this_artist
     song
   end
 
