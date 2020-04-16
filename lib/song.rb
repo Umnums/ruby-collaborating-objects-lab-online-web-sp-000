516c3388b471add713bda99f7d25e686770d13d9
Joseph Kao <josephtkao@gmail.com>
2020-04-16 03:12:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,4 +12,10 @@ class Song
     @@all
   end
 
+  def new_by_filename(file)
+    artist = file.strip(" - ")[0]
+    name = file.strip(" - ")[1]
+    song = Song.new(name)
+    song.artist = name
+
 end
