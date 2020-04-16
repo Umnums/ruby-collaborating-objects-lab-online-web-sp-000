373041b7dbb10a692670c1449f70e61e1741be60
Joseph Kao <josephtkao@gmail.com>
2020-04-16 03:37:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,9 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.include?{|song| song.name == name}
+    flag = nil
+    self.all.each do |song|
+      
       self.all.find{|song| song.name == name}
     else
       new_artist = Artist.new(name)
